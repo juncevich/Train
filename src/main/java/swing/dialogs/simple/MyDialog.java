be1fb7c360fce4896a694daebd46e9b4aa67e952
Александр Юнцевич <alexunc@hotmail.com>
2016-01-29 14:29:16 +0500

    Add SwingConsole.

--- src/main/java/swing/dialogs/simple/MyDialog.java
+++ src/main/java/swing/dialogs/simple/MyDialog.java
@@ -1,5 +1,20 @@
 package swing.dialogs.simple;
 
-public class MyDialog {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class MyDialog extends JDialog {
+    public MyDialog(JFrame parent) {
+        super(parent, "MyDialog", true);
+        setLayout(new FlowLayout());
+        add(new JLabel("Here is my dialog"));
+        JButton ok = new JButton("OK");
+        ok.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
 }
---
+++ src/main/java/swing/utils/SwingConsole.java
@@ -0,0 +1,18 @@
+package swing.utils;
+
+
+import javax.swing.*;
+
+public class SwingConsole {
+    public static void run(final JFrame f, final int width, final int heigth) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                f.setTitle(f.getClass().getSimpleName());
+                f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                f.setSize(width, heigth);
+                f.setVisible(true);
+            }
+        });
+    }
+}
