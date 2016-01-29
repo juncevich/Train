22be00a90c47f71548a710cf77bbf3d4db800a6d
Александр Юнцевич <alexunc@hotmail.com>
2016-01-29 21:16:42 +0500

    Add swing.dialogs.filechooser.

---
+++ src/main/java/swing/dialogs/filechooser/FileChooserTest.java
@@ -0,0 +1,74 @@
+package swing.dialogs.filechooser;
+
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static swing.utils.SwingConsole.run;
+
+public class FileChooserTest extends JFrame {
+    private JTextField
+            fileName = new JTextField(),
+            dir = new JTextField();
+    private JButton
+            open = new JButton("Open"),
+            save = new JButton("Save");
+    private FileChooserTest() {
+        JPanel p = new JPanel();
+        open.addActionListener(new OpenL());
+        p.add(open);
+        save.addActionListener(new SaveL());
+        p.add(save);
+        add(p, BorderLayout.SOUTH);
+        dir.setEditable(false);
+        fileName.setEditable(false);
+        p = new JPanel();
+        p.setLayout(new GridLayout(2, 1));
+        p.add(fileName);
+        p.add(dir);
+        add(p, BorderLayout.NORTH);
+    }
+
+    private class OpenL implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JFileChooser c = new JFileChooser();
+            int rVal = c.showOpenDialog(FileChooserTest.this);
+            if (rVal == JFileChooser.APPROVE_OPTION) {
+                fileName.setText(c.getSelectedFile().getName());
+                dir.setText(c.getCurrentDirectory().toString());
+            }
+            if (rVal == JFileChooser.CANCEL_OPTION) {
+                fileName.setText("You pressed cancel");
+                dir.setText("");
+            }
+        }
+    }
+    private class SaveL implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JFileChooser c = new JFileChooser();
+            int rVal = c.showOpenDialog(FileChooserTest.this);
+            if (rVal == JFileChooser.APPROVE_OPTION) {
+                fileName.setText(c.getSelectedFile().getName());
+                dir.setText(c.getCurrentDirectory().toString());
+            }
+            if (rVal == JFileChooser.CANCEL_OPTION) {
+                fileName.setText("You pressed cancel");
+                dir.setText("");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        run(new FileChooserTest(), 450, 250);
+    }
+
+}
