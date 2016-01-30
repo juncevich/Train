a3ebd288a3963aedbb81b832eb0291a702360c15
Александр Юнцевич <alexunc@hotmail.com>
2016-01-30 15:14:58 +0500

    Add algoritms.arrays.orderedarray .

---
+++ src/main/java/algoritms/orderedarray/OrdArray.java
@@ -0,0 +1,188 @@
+package algoritms.orderedarray;
+
+/**
+ * Class, tat show ordered array.
+ */
+class OrdArray {
+    /**
+     * ref to array a.
+     */
+    private final long[] a;
+    /**
+     * number of data items.
+     */
+    private int nElems;
+
+    /**
+     * Class constructor.
+     *
+     * @param max size of array.
+     */
+    OrdArray(final int max) {
+        a = new long[max];             // create array
+        nElems = 0;
+    }
+
+    /**
+     * Merge two arrays.
+     *
+     * @param firstArray  .
+     * @param secondArray .
+     * @return merged array, made from first and second array.
+     */
+    public static OrdArray merge(final OrdArray firstArray,
+                                 final OrdArray secondArray) {
+        OrdArray resultArray;
+        OrdArray supportArray;
+        if (firstArray.getElems() > secondArray.getElems()) {
+            resultArray = firstArray;
+            supportArray = secondArray;
+        } else {
+            resultArray = secondArray;
+            supportArray = firstArray;
+        }
+
+        for (int i = 0; i < supportArray.getElems(); i++) {
+            resultArray.insertOrdered(supportArray.getElement(i));
+        }
+
+        return resultArray;
+
+    }
+
+    /**
+     * Array size.
+     *
+     * @return amount of array elements.
+     */
+    public int size() {
+        return nElems;
+    }
+
+    /**
+     * Find current value.
+     *
+     * @param searchKey current value.
+     * @return founded value.
+     */
+    public int find(final long searchKey) {
+        int lowerBound = 0;
+        int upperBound = nElems - 1;
+        int curIn;
+
+        while (true) {
+            curIn = (lowerBound + upperBound) / 2;
+            if (a[curIn] == searchKey) {
+                return curIn;              // found it
+            } else {
+                if (lowerBound > upperBound) {
+                    return nElems;             // can't find it
+                } else {                         // divide range
+                    if (a[curIn] < searchKey) {
+                        lowerBound = curIn + 1; // it's in upper half
+                    } else {
+                        upperBound = curIn - 1; // it's in lower half
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * put element into array.
+     *
+     * @param value element.
+     */
+    public void insert(final long value) {
+        int j;
+
+        for (j = 0; j < nElems; j++) {       // find where it goes
+            if (a[j] > value) {           // (linear search)
+                break;
+            }
+        }
+        for (int k = nElems; k > j; k--) {   // move bigger ones up
+            a[k] = a[k - 1];
+        }
+        a[j] = value;                  // insert it
+        nElems++;                      // increment size
+    }
+
+    /**
+     * In this method used binary search.
+     *
+     * @param value value.
+     */
+    public void insertOrdered(final long value) {
+        int lowerBound = 0;
+        int upperBound = nElems - 1;
+        int curIn;
+        while (true) {
+
+            curIn = (lowerBound + upperBound) / 2;
+
+            if (a[curIn] < value) {
+                lowerBound = curIn + 1; // it's in upper half
+            } else {
+                upperBound = curIn - 1; // it's in lower half
+            }
+            if (value < a[curIn + 1] & value > a[curIn]) {
+                break;
+            }
+        }
+        for (int k = nElems; k > curIn; k--) {   // move bigger ones up
+            a[k] = a[k - 1];
+        }
+
+        a[curIn + 1] = value;
+        nElems++;
+    }
+
+    /**
+     * delete element from array.
+     *
+     * @param value element.
+     * @return result of operation.
+     */
+    public boolean delete(final long value) {
+        final int j = find(value);
+        if (j == nElems) {                 // can't find it
+            return false;
+        } else {                          // found it
+            for (int k = j; k < nElems; k++) {  // move bigger ones down
+                a[k] = a[k + 1];
+            }
+            nElems--;                   // decrement size
+            return true;
+        }
+    }
+
+    /**
+     * Get current element from array.
+     *
+     * @param value element index.
+     * @return element.
+     */
+    private long getElement(final int value) {
+        return a[value];
+    }
+
+    /**
+     * Return size of array.
+     * @return nElems.
+     */
+    private int getElems() {
+        return nElems;
+    }
+
+    /**
+     * Print array elements.
+     */
+    public void display() {            // displays array contents
+        for (int j = 0; j < nElems; j++) {       // for each element,
+            System.out.print(a[j] + " ");  // display it
+        }
+        System.out.println("");
+    }
+
+}
---
+++ src/main/java/algoritms/orderedarray/OrderedApp.java
@@ -0,0 +1,65 @@
+package algoritms.orderedarray;
+
+
+import static algoritms.orderedarray.OrdArray.merge;
+
+/**
+ * Application class.
+ */
+public class OrderedApp {
+    /**
+     * Main method.
+     *
+     * @param args args.
+     */
+    public static void main(final String[] args) {
+        int maxSize = 100;             // array size
+        OrdArray arr;                  // reference to array
+        arr = new OrdArray(maxSize);   // create the array
+
+        arr.insert(77);                // insert 10 items
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(0);
+        arr.insert(66);
+        arr.insert(33);
+
+        int searchKey = 55;            // search for item
+        if (arr.find(searchKey) != arr.size()) {
+            System.out.println("Found " + searchKey);
+        } else {
+            System.out.println("Can't find " + searchKey);
+        }
+        arr.display();                 // display items
+
+        arr.delete(0);                // delete 3 items
+        arr.delete(55);
+        arr.delete(99);
+
+        arr.display();                 // display items again
+        arr.insertOrdered(30);
+        arr.display();
+
+        OrdArray firstArray = new OrdArray(90);
+        OrdArray secondArray = new OrdArray(100);
+        firstArray.insert(1);
+        firstArray.insert(3);
+        firstArray.insert(5);
+        firstArray.insert(7);
+        firstArray.insert(9);
+        secondArray.insert(0);
+        secondArray.insert(2);
+        secondArray.insert(4);
+        secondArray.insert(6);
+        secondArray.insert(8);
+        secondArray.insert(10);
+
+        OrdArray resultArray = merge(firstArray, secondArray);
+        resultArray.display();
+        System.out.println();
+    }
+}
